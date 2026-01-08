183f89a85c67226fe01235d1f5efb708a9995d09
roijacob-msk <roi.olfindo@mysuki.io>
2026-01-08 17:31:39 +0800

    Add Sherlock risk segment column

--- data_extraction/sherlock_customers.R
+++ data_extraction/sherlock_customers.R
@@ -1,19 +1,34 @@
 library(readr)
 library(dplyr)
 library(googledrive)
+library(googlesheets4)
 library(purrr)
+library(stringr)
 
-sherlock_experimental_customers <- drive_ls("kasuki/2025-08-sherlock-experimental", type = "csv") |>
-  pull(id) |> 
-  map(
-    function(file_id) { drive_read_string(file_id) |> read_csv() }    
-  ) |> 
-  bind_rows()
+{
+  sherlock_run_august <- read_sheet("https://docs.google.com/spreadsheets/d/1qwTYn2Pvh4txtF1x0SkyOB1cweXtykDTrLY4Vp7i7qY/edit", sheet = "Borrower Details") |> 
+    select(customer_id, sherlock_segment = proba_group) |> 
+    mutate(sherlock_segment = str_replace_all(
+      sherlock_segment, "percent", "%"
+    ))
 
-sherlock_xmas_customers <- drive_ls("kasuki/2025-11-sherlock-seasonal-xmas", type = "csv") |>
+  sherlock_experimental_customers <- drive_ls("kasuki/2025-08-sherlock-experimental", type = "csv") |>
+    pull(id) |> 
+    map(function(file_id) { drive_read_string(file_id) |> read_csv() }) |> 
+    bind_rows() |> 
+    left_join(sherlock_run_august, by = "customer_id")
+}
+
+{
+  sherlock_run_november <- read_sheet("https://docs.google.com/spreadsheets/d/1MTUB1X2V6naRvBa6EZB0ahHIfHqB4g73Tyc4haEu4E8/edit", sheet = "Borrower List") |> 
+    select(customer_id, sherlock_segment)
+
+  sherlock_xmas_customers <- drive_ls("kasuki/2025-11-sherlock-seasonal-xmas", type = "csv") |>
     pull(id) |> 
     drive_read_string() |> 
-    read_csv()
+    read_csv() |> 
+    left_join(sherlock_run_november, by = "customer_id")      
+}
 
 sherlock_customers <- c(
   sherlock_experimental_customers$customer_id,
