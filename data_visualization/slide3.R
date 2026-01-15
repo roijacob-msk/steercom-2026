b2743cedd56fa0ac8835734e0b9491beccfb0829
roijacob-msk <roi.olfindo@mysuki.io>
2026-01-15 12:30:09 +0800

    Add data processing for 3rd slide

--- data_visualization/slide3.R
+++ data_visualization/slide3.R
@@ -1,9 +1,54 @@
+library(DBI)
+library(dplyr)
 library(ggeasy)
 library(ggplot2)
 library(ggrounded)
 library(patchwork)
 library(scales)
 
+# Processing ----
+non_sherlock_customers <- pay_later_txns |>
+  filter(
+    increase_type == "Non-Sherlock",
+    year_month == "2025-12"
+  ) |>
+  pull(customer_id) |>
+  unique()
+
+{
+  customer_ids_sql <- paste0("'", non_sherlock_customers, "'", collapse = ", ")
+
+  query <- glue::glue(
+    "
+    WITH pay_later_txn AS (
+      SELECT *
+        , ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY activated_at ASC) AS rn
+      FROM ksk_pay_later_transactions
+      WHERE vendor_id = '5ed13854c703830b3df5502a'
+        AND customer_id IN ({customer_ids_sql})
+    )
+
+    SELECT *
+    FROM pay_later_txn
+    WHERE rn = 1
+    "
+  )
+
+  result <- dbGetQuery(redshift, query)
+}
+
+non_sherlock_yearly_metrics <- result |>
+  select(customer_id, amount, activated_at) |>
+  mutate(year = strftime(activated_at, "%Y")) |>
+  summarize(
+    total_amount = sum(amount),
+    customers = n_distinct(customer_id),
+    .by = year
+  ) |>
+  mutate(gdp = total_amount / customers) |>
+  arrange(desc(year))
+
+# Visualization ----
 p1 <- ggplot(
   non_sherlock_yearly_metrics,
   aes(x = year, y = total_amount)
